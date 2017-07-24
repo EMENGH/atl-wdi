3551f71830ebf9ef333dd216931ebcbe039ddbf6
EMENGH <eamendez62@gmail.com>
2017-07-24 18:29:29 -0400

    PART2: added arguments to tamagotchi

--- tamagotchi.js
+++ tamagotchi.js
@@ -2,10 +2,12 @@
 
 //your constructor function here
 class Tamagotchi {
-   constructor(foodInTummy, restedness, health) {
+   constructor(name, creatureType, foodInTummy, restedness, health) {
        this.foodInTummy =10;
        this.restedness = 10;
        this.health = 10;
+       this.name = name;
+       this.creatureType = creatureType;
    }
 
    //add method called cry
@@ -13,15 +15,16 @@ class Tamagotchi {
      this.foodInTummy -= 1;
       console.log("food in tummy = " + this.foodInTummy);
       console.log("WAHHH");
+      console.log(this.name + "WAHHH");
    }    
 }
 //create new Tamagotchis
 
  const newTamag1 = new Tamagotchi("newTamag1");
  const newTamag2 = new Tamagotchi("newTamag2"); 
+ const newTamag3 = new Tamagotchi("newTamag3");
  
 //invoke each tamagotchi's cry method
   newTamag1.cry(24);
 
 //test out your Tamagotchies below via console.logs
-

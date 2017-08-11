cf220d53aa06640d436e752c1a3743da8346de93
EMENGH <eamendez62@gmail.com>
2017-08-11 11:50:42 -0400

    react router push

---
+++ reactBank/bank-of-react2/src/components/Credits.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import AccountBalance from './AccountBalance';
+import {Link} from 'react-router-dom';
+import Transactions from './Transactions'
+
+class Credits extends Component {
+  render() {
+    const credits = this.props.credits;
+
+    const CreditComponent = credits.map((credit, i) => {
+      return 
+         <Transactions
+             key={i}
+             amount={credit.amount}
+             date={credit.date}
+             description={credit.description}
+         />
+    })
+      return (
+         <div>
+            <h1>Credits</h1>
+            <h3>Account Balance: {this.props.accountBalance}</h3>
+            <Link to="/">Home</Link><Link to="/debits">   Debits</Link>
+            <h3>New Transaction</h3>
+            
+            <h2>Credit History</h2>
+            {CreditComponent}
+          </div>
+      );
+  }
+}
+export default Credits;
---
+++ reactBank/bank-of-react2/src/components/Debits.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import Transactions from './Transactions';
+import TransactionForm from './TransactionForm';
+import AccountBalance from './AccountBalance';
+
+class Debits extends Component {
+  render() {
+    const debits = this.props.debits;
+
+    const DebitComponent = debits.map((debit, i) => {
+      return
+       <Transactions
+         key={i}
+         amount={debit.amount}
+         date={debit.date}
+         description={debit.description}
+       />
+    })
+      return (
+       <div>
+         <h1>Debits</h1>
+         <h3>Account Balance: {this.props.accountBalance}</h3>
+         <Link to="/">Home</Link><Link to="/credits">   Credits</Link>
+         <h3>New Transaction</h3>
+         <TransactionForm newDebit={this.props.newDebit} />
+         <h2>Debit History</h2>
+            {DebitComponent}
+       </div>
+      );
+  }
+}
+
+export default Debits;
